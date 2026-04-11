bf63a1205755b1f22bbe94bc0654b64e883c4606
Samurai <146192205+KIRILL22113@users.noreply.github.com>
2026-04-11 16:04:10 +0300

    Добавление новых функций в клиент и сервер
    Ошибки на сервере, новый интерфейс с помощью библиотеки colorama, команда для выхода из программы клиенту.

--- client.py
+++ client.py
@@ -1,10 +1,28 @@
 import socket
+from colorama import Fore, Style, init
 
-client = socket.socket()
-client.connect(("localhost", 12345))
+def error(text):
+    print(Fore.RED + text + Style.RESET_ALL)
 
-want_send = True
+def success(text):
+    print(Fore.GREEN + text + Style.RESET_ALL)
 
-while want_send == True:
+def title(text):
+    print(Fore.YELLOW + text + Style.RESET_ALL)
+
+
+title("===TeChat===")
+print("Напишите /exit чтобы выйти")
+
+client = socket.socket() 
+client.connect(("localhost", 12345)) # Подключаемся к серверу
+
+while True:
     message = input("Введите сообщение: ")
-    client.send(message.encode())
+
+    if message == "/exit":
+        break
+
+    client.send(message.encode()) # Переводим сообщение в двоичный код и отправляем
+
+client.close()
--- server.py
+++ server.py
@@ -1,4 +1,14 @@
 import socket
+from colorama import Fore, Style, init
+
+def error(text):
+    print(Fore.RED + text + Style.RESET_ALL)
+
+def success(text):
+    print(Fore.GREEN + text + Style.RESET_ALL)
+
+def title(text):
+    print(Fore.YELLOW + text + Style.RESET_ALL)
 
 server = socket.socket()
 server.bind(("localhost", 12345))
@@ -6,8 +16,21 @@ server.listen()
 
 client, addr = server.accept()
 
-server_open = True
 
-while server_open == True:
-    data = client.recv(1024).decode()
-    print("Получено:", data)
+
+while True:
+    try:
+        data = client.recv(1024).decode()
+
+        if not data:
+            error("Клиент отключился")
+            break
+
+        print("Получено:", data)
+
+    except: 
+        error("Клиент отключился")
+        break
+
+client.close()
+server.close()
